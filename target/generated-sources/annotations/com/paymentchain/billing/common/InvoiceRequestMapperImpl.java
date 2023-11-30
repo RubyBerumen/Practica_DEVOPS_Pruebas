89d71210af832d2ed680c7946c083f19b7e83a4d
RubyBerumen <71149865+RubyBerumen@users.noreply.github.com>
2023-11-30 11:30:04 -0600

    cambios class

--- target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,12 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-<<<<<<< HEAD
-    date = "2023-11-28T11:32:00-0600",
-=======
-    date = "2023-11-28T10:20:22-0600",
->>>>>>> 30f46711c84635a518462d80fe2e6378d04e604a
-    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
+    date = "2023-11-30T11:25:48-0600",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.36.0.v20231030-1524, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
